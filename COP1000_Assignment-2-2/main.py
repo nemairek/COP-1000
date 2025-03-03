450cf93fa503ba603612578eb5e219901f506929
Kuziva Nemaire <66959171+nemairek@users.noreply.github.com>
2025-03-02 23:33:47 -0500

    Add files via upload

---
+++ COP1000_Assignment-2-2/main.py
@@ -0,0 +1,10 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Create your variables here
+
+myCurrentAge = 24
+currentYear = 2025
+birthYear = (currentYear - myCurrentAge)
+print(myCurrentAge, currentYear, birthYear )
