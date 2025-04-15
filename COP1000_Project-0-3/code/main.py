f74e2310d81cbd9bd40b215b3ce3c63e38613807
Kuziva Nemaire <66959171+nemairek@users.noreply.github.com>
2025-04-15 19:02:04 -0400

    Semi Finished Project 0-3

--- COP1000_Project-0-3/code/main.py
+++ COP1000_Project-0-3/code/main.py
@@ -1,9 +1,12 @@
+
 AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
 vehicleOption2 = ""
+vehicleOption3 = ""
 
-with open("COP1000_Project-0-3\data\car.txt", "r") as db:
-     options = db.read()
-     db.close()
+
+
+with open("COP1000_Project-0-3\data\car.txt", "w") as f:
+    f.write(str(AllowedVehiclesList))
 
 def menu():
     print("********************************")
@@ -22,10 +25,9 @@ menu()
 choice = int(input("Select Choice:"))
 
 if choice == 1:
-     vehicleOption2 = ""
      print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
      [print(vehicle) for vehicle in AllowedVehiclesList]
-     
+
 elif choice == 2: 
      vehicleOption2 = str(input("Please Enter the full Vehicle name:"))
 if vehicleOption2 == "":
@@ -38,16 +40,17 @@ elif vehicleOption2 not in AllowedVehiclesList:
      print("is not an authorized vehicle, if you received this in error please check the spelling and try again")
 
 if choice == 3:      
-     vehicleOption3 = str(input("Please Enter the full Vehicle name you would like to add:"))
-     for vehicle in AllowedVehiclesList:
-      AllowedVehiclesList.append(vehicleOption3)
-     print("You have added")
-     print(vehicleOption3)
-     print("as an authorized vehicle")
+ while True:
+     vehicleOption3 = input("Please Enter the full Vehicle name you would like to add: ")     
+     AllowedVehiclesList.append(str(vehicleOption3))
+     if vehicleOption3 == "stop":
+      break          
+ print("You have added", vehicleOption3,"as an authorized vehicle" )
+ [print(vehicle) for vehicle in AllowedVehiclesList]    
 elif choice == 4: 
         
-     print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-     quit()
+      print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+      quit()
 
 
 
